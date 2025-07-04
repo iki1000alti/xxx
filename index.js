1b0176f5a38c6c5595bf70e3eb17ccf35b6f6b1d
iki1000alti <iki1000alti@gmail.com>
2025-07-04 11:34:32 +0300

    guncelleme3

--- index.js
+++ index.js
@@ -16,7 +16,16 @@ const pool = mysql.createPool({
 });
 
 const app = express();
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(express.json());
 
 app.get('/', (req, res) => {
